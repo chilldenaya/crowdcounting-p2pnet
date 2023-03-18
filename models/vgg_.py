f09ec1003c01b0573c6008278cff23c531b19b37
Denaya <denaya.rahadika@pinhome.id>
2023-03-18 12:04:41 +0800

    add gitignore

--- models/vgg_.py
+++ models/vgg_.py
@@ -31,7 +31,6 @@ model_paths = {
 
 
 class VGG(nn.Module):
-
     def __init__(self, features, num_classes=1000, init_weights=True):
         super(VGG, self).__init__()
         self.features = features
